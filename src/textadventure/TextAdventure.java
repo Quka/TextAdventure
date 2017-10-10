22eab317c5dcf7c41bfce1fc7462672350d437b1
Alperen <Alperen@10.50.130.108>
2017-10-10 10:06:33 +0200

    jeg har sk....

--- src/textadventure/TextAdventure.java
+++ src/textadventure/TextAdventure.java
@@ -4,6 +4,8 @@ public class TextAdventure {
 
     public static void main(String[] args) {
         System.out.println("Arlind er sej");
+        
+        System.out.println("ALperen");
     }
 
 }
