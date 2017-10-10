2d0862291c0a4bc76fc5f559447a8ce81c69b43a
Alperen <Alperen@10.50.130.108>
2017-10-10 11:47:27 +0200

    start room class

--- src/textadventure/Room.java
+++ src/textadventure/Room.java
@@ -1,8 +1,35 @@
 package textadventure;
 
 public class Room {
+
+    private int id; //test for os selv indtil videre vi skal nok slette det Arlind <3
     
-    public Room() {
-        
+    private Room north;
+    private Room east;
+    private Room south;
+    private Room west;
+    
+    private boolean winGame;
+    private String description;
+    
+
+    public Room(Room north, Room east, Room south, Room west) {
+        id = 0; //for loop på room arraylength
+        description = "blabla"; //random decription
+        winGame = false;
+    }
+    
+    public String description() {
+
+    return "blabla";  //random description
+    }
+    
+    public Room getRoom(String direction){
+    return something;
+    }
+    
+    public boolean winGame(){
+    return id == 19; //last room in array, kantine
     }
 }
+    
