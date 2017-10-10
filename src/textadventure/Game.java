a64a72b3eb3c6f5f0373b2aa6b687b8f8b23acfb
Arlind Ukshini <quka93@gmail.com>
2017-10-10 11:49:27 +0200

    Start Game class

--- src/textadventure/Game.java
+++ src/textadventure/Game.java
@@ -1,12 +1,42 @@
 package textadventure;
 
+import java.util.ArrayList;
+import textio.*;
+
 public class Game {
-    
+    private TextIO io = new TextIO(new SysTextIO());
+    private ArrayList<Room> rooms = new ArrayList<>();
+            
     public Game() {
         
     }
-    
+
     public void play() {
+        io.put(startGame());
+        io.put("Indtast navn:");
+        String name = io.get();
+        io.put("Hej " + name + ", velkommen til Firma & Fiskefilet\n");
         
+        createRooms();
+    }
+    
+    public String startGame() {
+        return "****************************************************************\n"
+                + "** Det er lige blevet frokost, og det er fiskedag.\n"
+                + "** Du skal finde vej til kantinen inden alle fiskefileterne er udsolgt.\n"
+                + "** Alt det andet fisk lugter, derfor er det vigtigt du får fiskefilet\n"
+                + "** til frokost i dag\n"
+                + "****************************************************************\n";
+    }
+    
+    public void createRooms() {
+        
+        for (int i = 0; i < 20; i++) {
+            rooms.add(new Room());
+        }
+        
+        for (Room room : rooms) {
+            System.out.println(room);
+        }
     }
 }
