975d4d3c3d698df594e47d43fad5ff4ca40def49
Arlind Ukshini <quka93@gmail.com>
2017-10-11 07:53:50 +0200

    added setRooms, fixed constructor

--- src/textadventure/Room.java
+++ src/textadventure/Room.java
@@ -3,33 +3,60 @@ package textadventure;
 public class Room {
 
     private int id; //test for os selv indtil videre vi skal nok slette det Arlind <3
-    
+
     private Room north;
     private Room east;
     private Room south;
     private Room west;
-    
+
     private boolean winGame;
     private String description;
-    
 
-    public Room(Room north, Room east, Room south, Room west) {
+    public Room() {
         id = 0; //for loop på room arraylength
         description = "blabla"; //random decription
         winGame = false;
     }
-    
+
     public String description() {
 
-    return "blabla";  //random description
+        return "blabla";  //random description
     }
-    
-    public Room getRoom(String direction){
-    return something;
+
+    /**
+     * Sets the nearby rooms for this room object, params can be null
+     *
+     * @param north Room north for this room
+     * @param east Room east for this room
+     * @param south Room south for this room
+     * @param west Room west for this room
+     */
+    public void setRooms(Room north, Room east, Room south, Room west) {
+
     }
-    
-    public boolean winGame(){
-    return id == 19; //last room in array, kantine
+
+    public Room getRoom(String direction) {
+        Room returnRoom = null;
+        
+        switch (direction) {
+            case "north":
+                returnRoom = this.north;
+                break;
+            case "east":
+                returnRoom = this.east;
+                break;
+            case "south":
+                returnRoom = this.south;
+                break;
+            case "west":
+                returnRoom = this.west;
+                break;
+        }
+
+        return returnRoom;
+    }
+
+    public boolean winGame() {
+        return id == 19; //last room in array, kantine
     }
 }
-    
