ba000f4a00995905fcf8783c3c4a882ffd144f2a
Arlind Ukshini <quka93@gmail.com>
2017-10-11 07:46:43 +0200

    Create player class

--- src/textadventure/Player.java
+++ src/textadventure/Player.java
@@ -2,7 +2,17 @@ package textadventure;
 
 public class Player {
     
-    public Player() {
-        
+    private String name;
+    private int health;
+    private Room inRoom;
+    
+    public Player(String name, Room startingRoom) {
+        this.name = name;
+        this.health = 100;
+        this.inRoom = startingRoom;
+    }
+    
+    public void walk(String direction) {
+        // inRoom = getRoom(direction);
     }
 }
