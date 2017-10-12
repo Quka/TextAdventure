43cd5d2d61be1aed06b99ac3b9ff6dd6ed56eb92
Arlind Ukshini <quka93@gmail.com>
2017-10-12 11:58:15 +0200

    Clear up text format + small changes'

--- src/textadventure/Game.java
+++ src/textadventure/Game.java
@@ -23,19 +23,17 @@ public class Game {
         createRooms();
 
         p = new Player(name, rooms.get(0));
-
         boolean gameEnded = false;
 
         while (!gameEnded) {
-
             io.put(p.getCurrentRoom().getDescription() + "\n");
 
+            // Get a command from user
             command();
-            
+
             if (p.getCurrentRoom().isWinGame() == true) {
                 gameEnded = true;
             }
-
         }
         io.put("Game ended");
     }
@@ -94,36 +92,91 @@ public class Game {
     public void createRooms() {
 
         rooms.add(new Room(
-                "Du sidder på dit kontor. Du kigger på uret og opdager,\n" +
-                "at du er sent på den. WTF! FISKEDAG! Bare der er\n" +
-                "fiskefilet tilbage, når du når kantinen", false, 0));
-        
+                "Du sidder på dit kontor. Du kigger på uret og opdager,\n"
+                + "at du er sent på den. WTF! FISKEDAG! Bare der er\n"
+                + "fiskefilet tilbage, når du når kantinen", false, 0));
+
         rooms.add(new Room(
-                "Du bliver kort blændet af en kontorlampe, som peger\n" +
-                "lige mod døråbningen. Du ser en gammel dame. SHHH!!\n" +
-                "Det må være hende de omtaler som \"blinde, snaksaglige,\n"+
-                "pensionsparate Ruth\". Forundret over " +
-                "hvorfor en blind har lys tændt, (((lister du videre for at undgå at hun hører dig)))", false, 1));
-        rooms.add(new Room("Døren knirker som du åbner den. Et kopirum! Det burde du have set komme. Især fordi det var en glasdør.", false, 2));
-        rooms.add(new Room("Ups! Dametoilettet. Der hænger en klam stank i luften. Det må være Ruth, som har været i gang. (((Boolean om der er nogen derude)))", false, 3));
-        rooms.add(new Room("Det var ikke kantinen det her, men du finder tilgengæld kiks og kaffe til at lette sulten lidt (((+ runder)))", false, 4));
-        rooms.add(new Room("Du træder ind i et lokale, hvor et vigtigt møde med en potentiel kunde er i gang. Du bliver nødt til at lade "
-                + "som om, at du er en sekretær", false, 5));
-        rooms.add(new Room("Rummet er tomt, men Phillipa har glemt sin håndtaske. (((Du bliver nysgerrig eller !)))", false, 6));
-        rooms.add(new Room("Du kommer til at vade ind i IT-lokalet, hvor alle nørderne sidder. De snakker ikke om andet end Rick & Morty og hvordan Heroes 3 var det bedste i serien.(((miste runde, energi)))", false, 7));
-        rooms.add(new Room("Kopimaskinen summer stadig. Den er åbenbart lige blevet færdig. Du går nysgerrigt over og kigger i de udskrevne papirer. Det er chefens oplæg til spareforslag :o Du er nødt til at se om dit navn står på listen. Det koster en runde ekstra", false, 8));
-        rooms.add(new Room("Du vader ind på chefens kontor. På hans skrivebord sidder sekretæren Line. Chefen ser, at du opdager dem flirte. (((bliver han sur, flov ect)))", false, 9));
-        rooms.add(new Room("Tine er ved at skrive en indkøbsseddel, da hun skal have gæster. Hun undskylder sig dog med, at hun skal bruge det til et IT-program", false, 10));
-        rooms.add(new Room("Pedersen er på vej ud fra toilettet. Han vasker ikke fingre! Slut med at give ham hånden.", false, 11));
-        rooms.add(new Room("Du kommer ind i tekøkkenet, hvor du kan se Thomas har efterladt sin Red Bull. Den napper du selvfølgelig (((runde)))", false, 12));
-        rooms.add(new Room("Du kommer ind på herretoilettet. Du skal simpelthen tisse så meget, at fiskefileterne må vente lidt. Du åbner toiletdøren, men ser at Ronnie har glemt at låse døren! Du mister en runde", false, 13));
-        rooms.add(new Room("Viceværten sidder og swiper på Tinder. Du tænker \"er han ikke gift med hende Alice fra regnskabsafdelingen?\"(((hans reaktion))) ", false, 14));
-        rooms.add(new Room("Mødelokalet er tomt, men der står kopper og service fra sidste møde. Sikke et rod!(((boolean med oprydning. der kommer en ind, skal hun have hjælp eller ej)))", false, 15));
-        rooms.add(new Room("Tobias sidder med sit seneste blad om 3d-gaming, men du ved at han burde lave oplæg om et nyt TAG-koncept", false, 16));
-        rooms.add(new Room("OMG! Hvad er det syn?! KANTINEN!! Du klarede det! Du skynder dig op i køen lige foran ham den arrogante fra din "
-                + "afdeling. Da du når frem til fadet er der kun 4 (((dependant on rounds left))) fiskefileter tilbage. Du snupper alle 4!", true, 17));
-        rooms.add(new Room("Lisette står og pudrer næse. Hun opdager dig og langer dig en syngende lussing (((miste runde)))", false, 18));
-        rooms.add(new Room("Projektgruppen sidder i mødelokalet. Vil du forsøge at forsinke dem i at nå fiskefileterne i kantinen? (((Muligheder actions))", false, 19));
+                "Du bliver kort blændet af en kontorlampe, som peger\n"
+                + "lige mod døråbningen. Du ser en gammel dame. SHHH!!\n"
+                + "Det må være hende de omtaler som \"blinde, snaksaglige,\n"
+                + "pensionsparate Ruth\". Forundret over "
+                + "hvorfor en blind har lys tændt, (((lister du videre for at undgå at hun hører dig)))", false, 1));
+
+        rooms.add(new Room(
+                "Døren knirker som du åbner den. Et kopirum! Det burde\n "
+                + "du have set komme. Især fordi det var en glasdør.", false, 2));
+
+        rooms.add(new Room(
+                "Ups! Dametoilettet. Der hænger en klam stank i luften.\n"
+                + "Det må være Ruth, som har været i gang. (((Boolean om der er nogen derude)))", false, 3));
+
+        rooms.add(new Room(
+                "Det var ikke kantinen det her, men du finder tilgengæld\n"
+                + "kiks og kaffe til at lette sulten lidt (((+ runder)))", false, 4));
+
+        rooms.add(new Room(
+                "Du træder ind i et lokale, hvor et vigtigt møde med en potentiel\n"
+                + "kunde er i gang. Du bliver nødt til at lade som om, at du er en sekretær", false, 5));
+
+        rooms.add(new Room(
+                "Rummet er tomt, men Phillipa har glemt sin håndtaske. (((Du bliver nysgerrig eller !)))", false, 6));
+
+        rooms.add(new Room(
+                "Du kommer til at vade ind i IT-lokalet, hvor alle nørderne sidder.\n"
+                + "De snakker ikke om andet end Rick & Morty og hvordan Heroes 3 var\n"
+                + "det bedste i serien.(((miste runde, energi)))", false, 7));
+
+        rooms.add(new Room(
+                "Kopimaskinen summer stadig. Den er åbenbart lige blevet færdig.\n"
+                + "Du går nysgerrigt over og kigger i de udskrevne papirer. Det er\n"
+                + "chefens oplæg til spareforslag :o Du er nødt til at se om dit\n"
+                + "navn står på listen. Det koster en runde ekstra", false, 8));
+
+        rooms.add(new Room(
+                "Du vader ind på chefens kontor. På hans skrivebord sidder sekretæren\n"
+                + "Line. Chefen ser, at du opdager dem flirte. (((bliver han sur, flov ect)))", false, 9));
+
+        rooms.add(new Room(
+                "Tine er ved at skrive en indkøbsseddel, da hun skal have gæster.\n"
+                + "Hun undskylder sig dog med, at hun skal bruge det til et IT-program", false, 10));
+
+        rooms.add(new Room(
+                "Pedersen er på vej ud fra toilettet. Han vasker ikke fingre!\n"
+                + "Slut med at give ham hånden.", false, 11));
+
+        rooms.add(new Room(
+                "Du kommer ind i tekøkkenet, hvor du kan se Thomas har efterladt\n"
+                + "sin Red Bull. Den napper du selvfølgelig (((runde)))", false, 12));
+
+        rooms.add(new Room(
+                "Du kommer ind på herretoilettet. Du skal simpelthen tisse så meget,\n"
+                + "at fiskefileterne må vente lidt. Du åbner toiletdøren, men ser at Ronnie\n"
+                + "har glemt at låse døren! Du mister en runde", false, 13));
+
+        rooms.add(new Room(
+                "Viceværten sidder og swiper på Tinder. Du tænker \"er han ikke gift med\n"
+                + "hende Alice fra regnskabsafdelingen?\"(((hans reaktion))) ", false, 14));
+
+        rooms.add(new Room(
+                "Mødelokalet er tomt, men der står kopper og service fra sidste møde.\n"
+                + "Sikke et rod!(((boolean med oprydning. der kommer en ind, skal hun have hjælp eller ej)))", false, 15));
+
+        rooms.add(new Room(
+                "Tobias sidder med sit seneste blad om 3d-gaming, men du ved at han burde\n"
+                + "lave oplæg om et nyt TAG-koncept", false, 16));
+
+        rooms.add(new Room(
+                "OMG! Hvad er det syn?! KANTINEN!! Du klarede det! Du skynder dig op i køen\n"
+                + "lige foran ham den arrogante fra din afdeling. Da du når frem til fadet\n"
+                + "er der kun 4 (((dependant on rounds left))) fiskefileter tilbage. Du snupper alle 4!", true, 17));
+
+        rooms.add(new Room(
+                "Lisette står og pudrer næse. Hun opdager dig og langer dig en syngende lussing (((miste runde)))", false, 18));
+
+        rooms.add(new Room(
+                "Projektgruppen sidder i mødelokalet. Vil du forsøge at forsinke dem i at\n"
+                + "nå fiskefileterne i kantinen? (((Muligheder actions))", false, 19));
 
         rooms.get(0).setNorth(rooms.get(1));
 
