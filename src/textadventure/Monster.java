c2f2e3bfa155b7289bfb0f9b4609823b06451cdd
Ionsight <Ionsight@10.50.137.176>
2017-10-25 09:35:10 +0200

    småting

---
+++ src/textadventure/Monster.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textadventure;
+
+/**
+ *
+ * @author Ionsight
+ */
+public class Monster implements MainCharacter {
+
+    private String name;
+    private int roundsLeft;
+    private Room currentRoom;
+
+    public Monster(String name, Room startingRoom) {
+        this.name = name;
+        this.roundsLeft = 100;
+        this.currentRoom = startingRoom;
+    }
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void walk(String direction) {
+        currentRoom = currentRoom.getRoom(direction);
+    }
+
+    public boolean canWalk(String direction) {
+        return currentRoom.getRoom(direction) != null;
+    }
+}
