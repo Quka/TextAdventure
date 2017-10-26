00a33f97f36e45c4539b89f3645dfbab18533781
thomasfritzboger <thomasfritzboger@10.50.130.10>
2017-10-26 08:40:25 +0200

    inventory updated methods

--- src/textadventure/Inventory.java
+++ src/textadventure/Inventory.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package textadventure;
 
 import textadventure.Item.Item;
@@ -12,20 +11,56 @@ import java.util.ArrayList;
 /**
  * @author Arlind U. <arlind.u at cph-au53@cphbusiness.dk>
  */
-public class Inventory {
-    
+public class Inventory
+{
+
     private ArrayList<Item> items = new ArrayList<>();
     private int capacity;
-    
-    public Inventory() {
+    private Item item;
+
+    public Inventory()
+    {
         capacity = 10;
     }
-    
+
     // add to inventory
-    
+    private void addToInventory(Item item)
+    {
+        this.item = item;
+        items.add(item);
+    }
+
     // show inventory
-    
+    private void showInventory(ArrayList<Item> items)
+    {
+
+        for (Item item : items)
+        {
+            System.out.println(items); // invokes Item.toString()
+        }
+    }
+
     // use item
-    
+    private void useItem(Item item)
+    {
+
+        this.item = item;
+        items.remove(item);
+
+    }
+
     // remove item
+    private void removeItem(Item item)
+    {
+        this.item = item;
+        items.remove(item);
+
+    }
+
+    @Override
+    public String toString()
+    {
+        return items.toString();
+    }
+
 }
