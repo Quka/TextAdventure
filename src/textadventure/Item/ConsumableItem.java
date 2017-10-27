94a05769eb07ae92350cd3ac9a82b4e02bdd2303
Ionsight <Ionsight@10.50.137.176>
2017-10-27 10:20:12 +0200

    Javadoc

--- src/textadventure/Item/ConsumableItem.java
+++ src/textadventure/Item/ConsumableItem.java
@@ -10,14 +10,30 @@ import textadventure.Item.Item;
 import textadventure.Characters.Player;
 
 /**
+ * An item that can be eaten or drank
+ * 
  * @author Arlind U. <arlind.u at cph-au53@cphbusiness.dk>
  */
 public class ConsumableItem extends Item {
     
+    /**
+     * Contructs and item
+     * 
+     * @param name
+     * @param description
+     * @param roundsLeftModifier 
+     */
     public ConsumableItem(String name, String description, int roundsLeftModifier) {
         super(name, description, roundsLeftModifier);
     }
     
+    /**
+     * Changes round the for player p!
+     * 
+     * @param p 
+     */
+    
+    //BTW, shouldn't this be for whatever you call this method with, not specifically player p?
     public void changeRoundsLeft(Player p) {
         p.changeRounds(roundsLeftModifier);
     }
