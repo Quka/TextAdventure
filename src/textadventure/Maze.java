7ca6660b52aa580e5874d83fdfb76d932882947b
Ionsight <Ionsight@10.50.137.176>
2017-10-29 20:15:39 +0100

    Comments

--- src/textadventure/Maze.java
+++ src/textadventure/Maze.java
@@ -21,6 +21,7 @@ public class Maze {
 
     //Items that are placed in the rooms. Should probably be moved to somewhere else in the code.
     //Two descriptions: Both for when you take it AND when you use it?
+    //Neither description nor roundLeftModifier is shown when picking up items at the moment
     private final ConsumableItem biscuitAndCoke = new ConsumableItem("Kiks & Cola", "Du ser en "
             + "pakke Mariekiks og en Pepsi. Du kan næsten ikke lade være med at nappe dem.", 7);
     private final ConsumableItem coffee = new ConsumableItem("Kaffe", "Du føler dig træt, "
