405de8f51c9c69f2fa8c02e4b64fc05ae902fc63
thomasfritzboger <thomasfritzboger@10.50.130.8>
2017-10-30 19:09:38 +0100

    Create monsters som Arrayliste ok, items ikke færdig, da itemsliste mangler

--- src/textadventure/Characters/Monster.java
+++ src/textadventure/Characters/Monster.java
@@ -54,7 +54,25 @@ class Monster {
         return "Monster{" + "decription=" + decription + '}';
     }  
     
-    public ArrayList<Monster> createMonsters(){
+    // Kunne det være en ide, at lave en getDesription, og når vi går ind i et 
+    // rum, kunne der i getRoomDescription, indgå noget getMonsterDescription??
+    // Arraylisten med Item skal jo sættes ind, men da den ikke er lavet, er alle
+    // items bare sat til index1!
+    
+    public ArrayList<Monster> createMonsters(ArrayList<Item> items){
+        
+        monsters.add(new Monster("Tine", -3, items.get(1), items.get(1)));
+        monsters.add(new Monster("Tobias", -3, items.get(1), items.get(1)));
+        monsters.add(new Monster("Ronnie", -4, items.get(1), items.get(1)));
+        monsters.add(new Monster("Viceværten", -5, items.get(1), items.get(1)));
+        monsters.add(new Monster("Ruth", -2, items.get(1), items.get(1)));
+        monsters.add(new Monster("Phillipa", -4, items.get(1), items.get(1)));
+        monsters.add(new Monster("IT Nørderne", -7, items.get(1), items.get(1)));
+        monsters.add(new Monster("Pedersen", -5, items.get(1), items.get(1)));
+        monsters.add(new Monster("Pedersen", -3, items.get(1), items.get(1)));
+        monsters.add(new Monster("Lisette", -3, items.get(1), items.get(1)));
+        monsters.add(new Monster("Potentiel Kunde", -7, items.get(1), items.get(1)));
+        monsters.add(new Monster("Projektgruppen", -5, null, items.get(1)));
         
     return monsters;
     }
