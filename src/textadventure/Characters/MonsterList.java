7ead2c0358baf3a52946c1084087cf8d9fdfab69
thomasfritzboger <thomasfritzboger@10.50.130.8>
2017-10-31 11:04:04 +0100

    MonsterList class revised

--- src/textadventure/Characters/MonsterList.java
+++ src/textadventure/Characters/MonsterList.java
@@ -7,6 +7,7 @@ package textadventure.Characters;
 
 import java.util.ArrayList;
 import textadventure.Item.Item;
+import textadventure.ItemList;
 
 /**
  *
@@ -16,28 +17,29 @@ public class MonsterList
 {
 
     private ArrayList<Monster> monsters = new ArrayList<>();
-    
+    private ItemList itemList = new ItemList();
     
     public MonsterList()
     {
         
     }
     
-     public ArrayList<Monster> createMonsters(ArrayList<Item> items){
+     public ArrayList<Monster> createMonsters(){
         
          
-        monsters.add(new Monster("Tine", -3, items.get(1), items.get(1)));
-        monsters.add(new Monster("Tobias", -3, items.get(1), items.get(1)));
-        monsters.add(new Monster("Ronnie", -4, items.get(1), items.get(1)));
-        monsters.add(new Monster("Viceværten", -5, items.get(1), items.get(1)));
-        monsters.add(new Monster("Ruth", -2, items.get(1), items.get(1)));
-        monsters.add(new Monster("Phillipa", -4, items.get(1), items.get(1)));
-        monsters.add(new Monster("IT Nørderne", -7, items.get(1), items.get(1)));
-        monsters.add(new Monster("Pedersen", -5, items.get(1), items.get(1)));
-        monsters.add(new Monster("Pedersen", -3, items.get(1), items.get(1)));
-        monsters.add(new Monster("Lisette", -3, items.get(1), items.get(1)));
-        monsters.add(new Monster("Potentiel Kunde", -7, items.get(1), items.get(1)));
-        monsters.add(new Monster("Projektgruppen", -5, null, items.get(1)));
+         
+        monsters.add(new Monster("Tine", -3, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Tobias", -3, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Ronnie", -4, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Viceværten", -5, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Ruth", -2, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Phillipa", -4, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("IT Nørderne", -7, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Pedersen", -5, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Pedersen", -3, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Lisette", -3, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Potentiel Kunde", -7, itemList.getItem(0), itemList.getItem(0)));
+        monsters.add(new Monster("Projektgruppen", -5, itemList.getItem(0), itemList.getItem(0)));
         
     return monsters;
     }
