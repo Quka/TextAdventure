01ee8a8406635e941ee32935b19e444af3dc0788
Ionsight <Ionsight@10.50.137.229>
2017-11-02 13:15:44 +0100

    Small changes

--- src/textadventure/Characters/MonsterList.java
+++ src/textadventure/Characters/MonsterList.java
@@ -19,41 +19,41 @@ public class MonsterList {
 
     public MonsterList() {
         monsterList.add(new Monster("Tine", -3, itemList.getItem(9), itemList.getItem(16),
-                "Hun drejer stolen 180 grader og kigger dig direkte i øjnene. Du får et koldt gys.\n"
-                + "Hvordan opdagede hun dig?! Hvad har hun tænkt sig at gøre?! Hvilken\n"
-                + "ret har hun tænkt sig at servere i aften?! Mens alle disse spørgsmål\n"
-                + "suser rundt i dit hoved rejser hun sig lige pludselig op, smiler og\n"
+                "Hun drejer stolen 180 grader og kigger dig direkte i øjnene. Du får et koldt gys."
+                + "Hvordan opdagede hun dig?! Hvad har hun tænkt sig at gøre?! Hvilken"
+                + "ret har hun tænkt sig at servere i aften?! Mens alle disse spørgsmål"
+                + "suser rundt i dit hoved rejser hun sig lige pludselig op, smiler og"
                 + "siger, uoverbevisende: \"Jeg skal bruge det til et IT-program\"", false));//
         monsterList.add(new Monster("Tobias", -3, itemList.getItem(10), itemList.getItem(14),
-                "Tobias sidder med sit seneste blad om 3D-gaming, men du ved at han burde\n"
-                + "lave oplæg om et nyt TAG-koncept. I et øjebliks hjerneafkopning påpeger\n"
+                "Tobias sidder med sit seneste blad om 3D-gaming, men du ved at han burde"
+                + "lave oplæg om et nyt TAG-koncept. I et øjebliks hjerneafkopning påpeger"
                 + "du dette overfor ham. Det skulle du aldrig have gjort", false));
         monsterList.add(new Monster("Ronnie", -4, itemList.getItem(11), null,
-                "Hvis han ikke så presset ud før, så gør han det i hvert fald, da han ser dig!\n"
-                + "Han ligner en, som ikke ved om han skal spørge efter noget eller skrige\n"
+                "Hvis han ikke så presset ud før, så gør han det i hvert fald, da han ser dig!"
+                + "Han ligner en, som ikke ved om han skal spørge efter noget eller skrige"
                 + "af sine lungers fulde kraft.", false));
         monsterList.add(new Monster("Viceværten", -5, itemList.getItem(13), null,
-                "Som ordsproget lyder: \"Det er altid en god idé at være på god fod med\n"
+                "Som ordsproget lyder: \"Det er altid en god idé at være på god fod med"
                 + "en vicevært\". Hvilket ordsprog spørger du? Dette ordsprog.", false));
         monsterList.add(new Monster("Ruth", -2, itemList.getItem(14), null,
-                "Det må være hende, som de omtaler som den \"blinde, for-altid-snakkende,\n"
-                + "pensionistparate sæk\". Du er forundret over hvorfor hun\n"
+                "Det må være hende, som de omtaler som den \"blinde, for-altid-snakkende,"
+                + "pensionistparate sæk\". Du er forundret over hvorfor hun"
                 + "har lys tændt. Du prøver at liste forbi hende, men hun hører dig.", false));
         monsterList.add(new Monster("Phillipa", -4, itemList.getItem(15), null,
-                "Hun begynder at flirte med dig. Hvis bare hun havde sin mobil, så \n"
+                "Hun begynder at flirte med dig. Hvis bare hun havde sin mobil, så"
                 + "hun ikke forstyrrer dig. Hvor mon mobilen egentlig er?", false));
         monsterList.add(new Monster("IT Nørderne", -7, itemList.getItem(16), itemList.getItem(10),
-                "Lyset fra døren lyser fanger alles øjne og blænder dem kortvarigt. Det ligner\n"
+                "Lyset fra døren lyser fanger alles øjne og blænder dem kortvarigt. Det ligner"
                 + "ikke, at de havde forventet besøg. Og de ser ikke glade ud!", false));
         monsterList.add(new Monster("Pedersen", -5, itemList.getItem(17), null,
-                "Han opdager, at du kigger på ham med afsky. I ren hævngerrighed over den\n"
+                "Han opdager, at du kigger på ham med afsky. I ren hævngerrighed over den"
                 + "dårlige samvittighed, som du har givet ham, stikker hansine hænder op mod dit ansigt", false));
         monsterList.add(new Monster("Lisette", -3, itemList.getItem(18), itemList.getItem(17),
-                "Du når lige at høre rædselsskriget. I ren slowmo matrix-style ser du en\n"
+                "Du når lige at høre rædselsskriget. I ren slowmo matrix-style ser du en"
                 + "syngende lussing komme flyvende i mod dig. Du kigger på hendes øjne.", false));
         monsterList.add(new Monster("Potentiel Kunde", -7, itemList.getItem(19), null,
-                "Kunden beder dig om at pudse hans sko. Uforskammet. Sådane en type\n"
-                + "har du simpelthen ikke tid til. Det eneste han tænker på er sgu da\n"
+                "Kunden beder dig om at pudse hans sko. Uforskammet. Sådane en type"
+                + "har du simpelthen ikke tid til. Det eneste han tænker på er sgu da"
                 + "også kun penge eller magt!", false));
         monsterList.add(new Monster("Projektgruppen", -5, null, null,
                 "De kigger på dig. Du kigger på dem. Akavet.", false));
